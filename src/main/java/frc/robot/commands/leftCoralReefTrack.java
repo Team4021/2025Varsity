38920b36961bbfa669954d431e213e5b5dc6ee52
Eli <Eli@lutherhigh.org>
2025-03-19 19:28:32 -0500

    left coral didnt go along with old push?

--- src/main/java/frc/robot/commands/leftCoralReefTrack.java
+++ src/main/java/frc/robot/commands/leftCoralReefTrack.java
@@ -1,44 +1,50 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.vision.LimelightSubsystem;
 import frc.robot.subsystems.vision.VisionConstants;
+import frc.robot.subsystems.vision.Vision;
 
 /** Command that uses AprilTags to Align and Drive towards Coral Station */
 public class leftCoralReefTrack extends Command {
-  private LimelightSubsystem m_Limelight;
-  public double aprilTagAlignment = 0;
-
-  public PIDController alignedPID =
-      new PIDController(
-          VisionConstants.AlignmentConstants.kP,
-          VisionConstants.AlignmentConstants.kI,
-          VisionConstants.AlignmentConstants.kD);
-
-  // The subsystem the command runs on
-  // private final LimelightSubsystem m_LimelightSubsystem;
-  public leftCoralReefTrack(LimelightSubsystem m_Limelight) {
-    // m_LimelightSubsystem = subsystem;
-    this.m_Limelight = m_Limelight;
-    addRequirements(m_Limelight);
-  }
-
-  @Override
+  private Vision m_vision;
+  public  double aprilTagAlignment;
+  
+    public  PIDController alignedPID =
+             new PIDController(
+                 VisionConstants.AlignmentConstants.kP,
+                 VisionConstants.AlignmentConstants.kI,
+                 VisionConstants.AlignmentConstants.kD);
+       
+         // The subsystem the command runs on
+         // private final LimelightSubsystem m_LimelightSubsystem;
+  public leftCoralReefTrack(Vision m_vision) {
+           // m_LimelightSubsystem = subsystem;
+           this.m_vision = m_vision;
+           addRequirements(m_vision);
+         }
+       
+         @Override
   public void initialize() {
-    aprilTagAlignment = 0;
-  }
-
-  public void execute() {
-    aprilTagAlignment =
-        alignedPID.calculate(
-            RobotContainer.Vision.getTargetX(0), RobotContainer.Vision.calculatecoralOffsetX());
+           aprilTagAlignment = 0;  
+           VisionConstants.AlignmentConstants.aprilTagAlignmentX = 0;
+           VisionConstants.AlignmentConstants.aprilTagAlignmentY = 0;
+         }
+       
+  public  void calculateAlignment() {
+    aprilTagAlignment = alignedPID.calculate(m_vision.getTargetX(0).getRadians(), m_vision.calculateLeftCoralOffsetX());
+    VisionConstants.AlignmentConstants.aprilTagAlignmentX = (-aprilTagAlignment * Constants.lastValidTargetAngle.getCos());
+    VisionConstants.AlignmentConstants.aprilTagAlignmentY = (-aprilTagAlignment * Constants.lastValidTargetAngle.getSin());
   }
 
   @Override
   public boolean isFinished() {
     aprilTagAlignment = 0;
+    VisionConstants.AlignmentConstants.aprilTagAlignmentX = 0;
+    VisionConstants.AlignmentConstants.aprilTagAlignmentY = 0;
     return true;
   }
 }
